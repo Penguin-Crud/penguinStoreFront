55150e94582be9e4ddea3ff54a870a776aa5dd28
unknown <patrasioo69@gmail.com>
2022-01-27 15:57:12 +0100

    added delete and refresh page

--- src/components/Penguin.js
+++ src/components/Penguin.js
@@ -2,27 +2,36 @@ import React, {useEffect, useState} from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import {Container,Paper,Button} from '@material-ui/core';
+import Gif from '../assets/pinguino.gif'
+import { LaptopWindowsRounded } from '@material-ui/icons';
 
 export default function Penguin() {
-    const paperStyle={padding:'50px 20px', width:600, margin:'20px Auto'}
-    const[nameSpecie,setNameSpecie]=useState('')
-    const[price,setPrice]=useState('')
-    const[description,setDescription]=useState('')
-    const[penguins,setPenguins]=useState([])
+  const paperStyle={padding:'50px 20px', width:600, margin:'20px Auto'}
+  const[nameSpecie,setNameSpecie]=useState('')
+  const[price,setPrice]=useState('')
+  const[description,setDescription]=useState('')
+  const[penguins,setPenguins]=useState([])
 
-    const handleClick=(e)=>{
-        e.preventDefault()
-        const penguin={nameSpecie,price,description}
-        console.log(penguin)
-        fetch("http://localhost:8080/penguin/add",{
-          method:"POST",
-          headers:{"Content-Type":"application/json"},
-          body:JSON.stringify(penguin)
 
-        }).then( () =>{
-          console.log("New Penguin added")
-        })
-    }
+  function refresh(){
+    window.location.reload(true);
+  }
+  
+  const handleClick = (e) =>{
+      e.preventDefault()
+      const penguin={nameSpecie,price,description}
+      console.log(penguin)
+      fetch("http://localhost:8080/penguin/add",{
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify(penguin)
+
+      }).then( () =>{
+        console.log("New Penguin added")
+        refresh();
+      })
+
+  }
 
   useEffect(()=>{
     fetch("http://localhost:8080/penguin/getAll")
@@ -31,7 +40,18 @@ export default function Penguin() {
       setPenguins(result);
     }
     )
-  },[]) 
+  },[])
+  
+
+  const deleteById = id =>{
+
+    fetch("http://localhost:8080/penguin/" + id, {method:"DELETE"})
+    .then( () => {
+      console.log("delete successful") 
+      refresh();
+    });
+
+  }
     
   return (
     <Box
@@ -42,7 +62,7 @@ export default function Penguin() {
     >
       <Container >
         <Paper elevation={3} style={paperStyle}>
-                <h1 style={{color:'blue'}}>Add student</h1>
+                <h1 style={{color:'blue'}}>Add Penguin</h1>
                 <TextField id="outlined-basic1" label="specie name" variant="outlined" fullWidth
                 value={nameSpecie}
                 onChange={(e)=>setNameSpecie(e.target.value)}/>
@@ -55,10 +75,12 @@ export default function Penguin() {
                 value={description}
                 onChange={(e)=>setDescription(e.target.value)}/>
 
-                <Button onClick={handleClick}>Submit</Button>
+                <Button variant="contained" color="secondary" onClick={handleClick}>Submit</Button>
         </Paper>
 
         <h1>Penguins</h1>
+
+        <img src={Gif} alt="loading..." />
 
         <Paper elevation={3} style={paperStyle}>
 
@@ -68,6 +90,9 @@ export default function Penguin() {
               NameSpecie:{penguin.nameSpecie}<br/>
               Price:{penguin.price}<br/>
               Description:{penguin.description}
+
+              <Button variant="contained" color="primary" >Edit</Button>
+              <Button variant="contained" color="secondary" onClick={() => deleteById(penguin.id)}>Delete</Button>
 
             </Paper>
           ))}
